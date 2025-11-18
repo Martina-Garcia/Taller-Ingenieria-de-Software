9745c6d4ffdc848299aa7c71483c17469791f75c
WENABOKI <manuel.morales@alumnos.ucn.cl>
2025-11-17 23:41:28 -0300

    nuevo

--- README.md
+++
@@ -1,5 +0,0 @@
-DOCUMENTACION
-To do
-Configurar .env
-
-dentro del front npm start
--- ecommerce-shop/src/components/Notificacion.jsx
+++
@@ -1,5 +0,0 @@
-const SimpleToast = () => {
-  return null; // si no quieres notificaciones aún
-};
-
-export default SimpleToast;
